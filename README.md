0f01ffdf4f791cdd665a2a6ee6781b2543dfd52b
cookfront <cookfront@gmail.com>
2016-05-16 11:10:29 +0800

    readme and update git hook shell

--- README.md
+++ README.md
@@ -21,6 +21,18 @@ gem install scss_lint
  - [eslint](http://eslint.org/docs/rules/)
  - [scss](https://github.com/brigade/scss-lint/blob/master/lib/scss_lint/linter/README.md)
 
+## 数据统计接口配置
+
+修改`pre-commit`和`commit-msg`钩子文件中`curl`对应行的接口请求地址，以及去掉注释。
+
+例如`commit-msg`：
+
+```c
+#!/bin/sh
+
+curl -G -s -o /dev/null "www.interface.com/commit/msg?user=$(git config user.name)" --data-urlencode "comments=$(cat $1)"
+```
+
 ## 工具
 
 ### html-css-js prettify
--- update_git_hooks.sh
+++ update_git_hooks.sh
@@ -21,11 +21,13 @@ cp ./.scss-lint.yml ..
 printf '\n\n========== chmod hook ==========\n'
 chmod a+x ./pre-commit
 chmod a+x ./pre-push
+chmod a+x ./commit-msg
 
 printf '\n\n========== ln hook ==========\n'
 cd ../.git/hooks/
 rm -f pre-commit pre-push post-merge commit-msg
 ln -sv ../../.git_hooks/pre-commit .
 ln -sv ../../.git_hooks/pre-push .
+ln -sv ../../.git_hooks/commit-msg .
 
 printf '\n\n如eslint和eslint-plugin-react未安装成功：\033[32m npm install -g eslint && npm install -g eslint-plugin-react\033[0m\n'
