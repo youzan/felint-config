6a766bc213e428ddeb2f8a63dfdae9d1c2f20a2e
delai <shidelai@gmail.com>
2016-05-18 17:43:29 +0800

    add check scss_Lint

--- update_git_hooks.sh
+++ update_git_hooks.sh
@@ -8,6 +8,13 @@ if [[ "$?" == 1 ]]; then
   npm install -g eslint-plugin-react
 fi
 
+# Check for scss_lint
+which scss-lint &> /dev/null
+if [[ "$?" == 1 ]]; then
+  printf '\n========== Install scss_lint ==========\n'
+  gem install scss_lint
+fi
+
 # cd to hooks folder
 isInHooks=$(pwd | grep '.git_hooks')
 if [[ $isInHooks == '' ]]; then
@@ -30,4 +37,5 @@ ln -sv ../../.git_hooks/pre-commit .
 ln -sv ../../.git_hooks/pre-push .
 ln -sv ../../.git_hooks/commit-msg .
 
-printf '\n\n如eslint和eslint-plugin-react未安装成功：\033[32m npm install -g eslint && npm install -g eslint-plugin-react\033[0m\n'
+printf '\n\n如eslint和eslint-plugin-react未安装成功：\033[32m npm install -g eslint && npm install -g eslint-plugin-react\033[0m'
+printf '\n\n如 scss_lint 未安装成功：\033[32m gem install scss_lint \033[0m\n'
