ef47fd33212c1b1e06abe7a7def92abe59e662ac
lianchengjie <ctyuna@gmail.com>
2016-06-20 17:59:17 +0800

    scss check on & rewrite update_git_hooks.sh

--- update_git_hooks.sh
+++ update_git_hooks.sh
@@ -1,42 +1,91 @@
 #!/bin/bash
 
+esV=$1
+projectPath=`pwd`
+
+# install package
+installPackage()
+{
+  pn=$1
+  printf "\n========== Install ${pn} start ==========\n"
+  npm install -g "$pn"
+  printf "\n========== Install ${pn} done ==========\n"
+}
+
+# check if package is already installed
+# if not install it
+checkAndInstallPackage()
+{
+  pn=$1
+  v=$2
+  printf "\n========== Check ${pn} ==========\n"
+  if [[ $packageList == '' ]]; then
+    packageList=`npm list -g --depth=0 2> /dev/null`
+  fi
+  echo "$packageList" | grep "$pn""@"
+  if [[ $? == '1' ]]; then
+    installPackage "${pn}""${v}"
+  fi
+}
+
 # Check for eslint
 which eslint &> /dev/null
 if [[ "$?" == 1 ]]; then
-  printf '\n========== Install eslint ==========\n'
-  npm install -g eslint
-  npm install -g eslint-plugin-react
+  installPackage "eslint@2.11.1"
 fi
 
 # Check for scss_lint
 which scss-lint &> /dev/null
 if [[ "$?" == 1 ]]; then
-  printf '\n========== Install scss_lint ==========\n'
-  gem install scss_lint
+  printf '\n========== Install scss_lint@0.48.0 start ==========\n'
+  gem install scss_lint --version=0.48.0
+  printf '\n========== Install scss_lint@0.48.0 done ==========\n'
+fi
+
+# Check eslint-plugin-react
+checkAndInstallPackage 'eslint-plugin-react' '@5.1.1"
+
+if [[ $esV == "6" ]]; then
+  checkAndInstallPackage 'babel-eslint' '@6.0.4'
+  checkAndInstallPackage 'eslint-plugin-import' '@1.8.1'
+  checkAndInstallPackage 'eslint-plugin-jsx-a11y' '@1.2.3'
+  checkAndInstallPackage 'eslint-config-airbnb' '@9.0.1'
 fi
 
 # cd to hooks folder
-isInHooks=$(pwd | grep '.git_hooks')
-if [[ $isInHooks == '' ]]; then
-	cd .git_hooks
+cd ~/.git_hooks
+
+printf '\n========== init .eslintrc start ==========\n'
+if [[ $esV == "6" ]]; then
+  # use es6 config
+  cp ./.eslintrc_es6 "${projectPath}/.eslintrc"
+else
+  # use es5 config
+  cp ./.eslintrc_es5 "${projectPath}/.eslintrc"
 fi
+printf '\n========== init .eslintrc done ==========\n'
 
-printf '\n========== init .eslintrc ==========\n'
-cp ./.eslint* ..
-cp ./.scss-lint.yml ..
+printf '\n========== init .scss-lint.yml start ==========\n'
+cp ./.scss-lint.yml "$projectPath"
+printf '\n========== init .scss-lint.yml done ==========\n'
 
-printf '\n\n========== chmod hook ==========\n'
-chmod a+x ./pre-commit
-chmod a+x ./pre-push
-chmod a+x ./commit-msg
+printf '\n========== init hook ==========\n'
+mkdir "${projectPath}/.git/hooks/"
+hooks="${projectPath}/.git/hooks/"
+rm -f "${hooks}/pre-commit" "${hooks}/pre-push" "${hooks}/commit-msg"
+cp ./pre-commit "$hooks"
+cp ./pre-push "$hooks"
+cp ./commit-msg "$hooks"
+printf '\n========== chmod hook ==========\n'
+chmod a+x "${hooks}/pre-commit"
+chmod a+x "${hooks}/pre-push"
+chmod a+x "${hooks}/commit-msg"
+printf '\n========== chmod hook done ==========\n'
 
-printf '\n\n========== ln hook ==========\n'
-mkdir ../.git/hooks/
-cd ../.git/hooks/
-rm -f pre-commit pre-push post-merge commit-msg
-ln -sv ../../.git_hooks/pre-commit .
-ln -sv ../../.git_hooks/pre-push .
-ln -sv ../../.git_hooks/commit-msg .
+printf '\n========== init hook done ==========\n'
+
+
 
 printf '\n\n如eslint和eslint-plugin-react未安装成功：\033[32m npm install -g eslint && npm install -g eslint-plugin-react\033[0m'
 printf '\n\n如 scss_lint 未安装成功：\033[32m gem install scss_lint \033[0m\n'
+printf '\n========== ALL DONE, THANKS\n'
