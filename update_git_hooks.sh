8ee8fbf7a5f9b9bd6562eae901af94f48ec40290
lianchengjie <lianchengjie@youzan.com>
2017-02-04 11:50:11 +0800

    安装依赖的时候判断版本

--- update_git_hooks.sh
+++ update_git_hooks.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 projectPath=`pwd`
+RED='\033[0;31m'
+NC='\033[0m'
+LGREEN='\033[1;32m'
+my_dir="$(dirname "$0")"
 
 # install package
 installPackage()
@@ -17,21 +21,22 @@ checkAndInstallPackage()
 {
   pn=$1
   v=$2
-  printf "\n========== Check ${pn} ==========\n"
   if [[ $packageList == '' ]]; then
-    packageList=`npm list -g --depth=0 2> /dev/null`
+    packageList=`npm list -g --depth=0 --silent --parseable=true 2> /dev/null`
   fi
-  echo "$packageList" | grep "$pn""@"
-  if [[ $? == '1' ]]; then
-    installPackage "${pn}""${v}"
+  sh "$my_dir/checkPackage.sh" "$pn" "$v" "$packageList"
+  result=$?
+  if [[ $result == '1' ]]; then
+    printf "${RED}${pn}包版本错误，可能会导致felint问题，推荐使用${LGREEN}${v}版本${NC}"
+  fi
+
+  if [[ $result == '2' ]]; then
+    installPackage "${pn}""@${v}"
   fi
 }
 
 # Check for eslint
-which eslint &> /dev/null
-if [[ "$?" == 1 ]]; then
-  installPackage "eslint@2.11.1"
-fi
+checkAndInstallPackage 'eslint' '2.11.1'
 
 # Check for scss_lint
 which scss-lint &> /dev/null
@@ -42,11 +47,12 @@ if [[ "$?" == 1 ]]; then
 fi
 
 # Check eslint-plugin-react
-checkAndInstallPackage 'eslint-plugin-react' '@5.1.1'
-checkAndInstallPackage 'babel-eslint' '@6.0.4'
-checkAndInstallPackage 'eslint-plugin-import' '@1.8.1'
-checkAndInstallPackage 'eslint-plugin-jsx-a11y' '@1.2.3'
-checkAndInstallPackage 'eslint-config-airbnb' '@9.0.1'
+checkAndInstallPackage 'eslint-plugin-react' '5.1.1'
+checkAndInstallPackage 'babel-eslint' '6.0.4'
+checkAndInstallPackage 'eslint-plugin-import' '1.8.1'
+checkAndInstallPackage 'eslint-plugin-jsx-a11y' '1.2.3'
+checkAndInstallPackage 'eslint-config-airbnb' '9.0.1'
+checkAndInstallPackage 'eslint-plugin-lean-imports' '0.3.3'
 
 # cd to hooks folder
 cd ./.felint
@@ -74,4 +80,5 @@ printf '\n========== init hook done ==========\n'
 
 printf '\n\n如eslint和eslint-plugin-react未安装成功：\033[32m npm install -g eslint && npm install -g eslint-plugin-react\033[0m'
 printf '\n\n如 scss_lint 未安装成功：\033[32m gem install scss_lint \033[0m\n'
+
 printf '\n========== ALL DONE, THANKS\n'
