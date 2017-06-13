eee5a776ad5aa3ba2579e41489e92172f497136b
lianchengjie <lianchengjie@youzan.com>
2017-06-13 16:34:23 +0800

    feat: 增加vue文件校验支持

--- config.js
+++ config.js
@@ -11,7 +11,8 @@ module.exports = {
             "eslint-plugin-react": "6.10.3",
             "eslint-plugin-vue": "2.0.1",
             "stylelint": "7.10.1",
-            "stylelint-config-standard": "16.0.0"
+            "stylelint-config-standard": "16.0.0",
+            "stylelint-processor-html": "1.0.0"
         }
     },
     plan: {
