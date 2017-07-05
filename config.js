c036b17b2ae0a1cce68c447c54db02025aee3700
lianchengjie <lianchengjie@youzan.com>
2017-07-05 15:10:17 +0800

    fix: 升级stylelint版本

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
             "eslint-plugin-lean-imports": "0.3.3",
             "eslint-plugin-react": "6.10.3",
             "eslint-plugin-vue": "2.0.1",
-            "stylelint": "7.10.1",
+            "stylelint": "7.12.0",
             "stylelint-config-standard": "16.0.0",
             "stylelint-processor-html": "1.0.0"
         }
